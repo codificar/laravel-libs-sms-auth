cafc4deda316548c515ca66cae18ec0b3d340aba
Álvaro Oliveira <alvaro.oliveira@codificar.com.br>
2020-08-12 20:11:21 -0300

    Correção rotas

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,7 @@
 use Codificar\Sms\SmsController;
 
 
-Route::group(['namespace' => 'api\v1', 'prefix' => '/api/v1/sms'], function () {
+Route::group(['prefix' => '/api/v1/sms'], function () {
     Route::get('request', SmsController::class . '@requestLogin');
     Route::get('login', SmsController::class . '@login');
 });
