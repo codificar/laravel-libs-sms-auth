2c091e6809d1d6407f0bcab45fcd4484ddf973ff
Ricardo Cavalcanti <ricardo.cavalcanti@codificar.com.br>
2021-09-20 12:05:37 -0300

    added the token active function to the model

--- src/Models/SmsCode.php
+++ src/Models/SmsCode.php
@@ -4,6 +4,7 @@ namespace Codificar\Sms\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class SmsCode extends Model
 {
 
@@ -16,7 +17,7 @@ class SmsCode extends Model
 	public static function makeSmsCode($phone)
 	{
 		$model = self::getByPhone($phone);
-		if(!$model) $model = new SmsCode(['phone' => $phone]);
+		if (!$model) $model = new SmsCode(['phone' => $phone]);
 		$model->code = self::generate_sms_code();
 		$model->code_expiry = self::generate_expiry(self::duration);
 		$model->save();
@@ -27,13 +28,13 @@ class SmsCode extends Model
 	{
 		return self::where('phone', 'like', $phone)
 			->orWhere('phone', 'like', str_ireplace('+55', '', $phone))
-			->orWhere('phone', 'like', '%'.$phone)
+			->orWhere('phone', 'like', '%' . $phone)
 			->first();
 	}
 
 	public function validate($code)
 	{
-		return $this->code == $code and is_token_active($this->code_expiry);
+		return $this->code == $code and self::is_token_active($this->code_expiry);
 	}
 
 
@@ -48,7 +49,12 @@ class SmsCode extends Model
 		return time() + ($duration * 60);
 	}
 
+	public static function is_token_active($ts)
+	{
+		if ($ts >= time()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
-
-
-
